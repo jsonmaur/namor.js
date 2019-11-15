1ec6a6972eefe5077944f5f741608c0bd0d22d95
Jason Maurer <jason@maur.co>
2019-11-15 02:34:20 -0700

    Update example site

--- example/index.js
+++ example/index.js
@@ -1,28 +1,29 @@
-const http = require('http')
-const url = require('url')
-const namor = require('namor')
+const http = require("http")
+const url = require("url")
+const namor = require("namor")
 
 const server = http.createServer((req, res) => {
-  const { query } = url.parse(req.url, true) // eslint-disable-line
+	const { query } = url.parse(req.url, true)
 
-  const payload = JSON.stringify({
-    generated_name: namor.generate({
-      words: query.words || 2,
-      saltLength: query.saltLength || 2,
-      separator: query.separator,
-      subset: query.subset,
-    })
-  }, null, 2)
+	const payload = JSON.stringify(
+		{
+			generated_name: namor.generate({
+				words: query.words || 2,
+				saltLength: query.saltLength || 2,
+				separator: query.separator,
+				subset: query.subset,
+			}),
+		},
+		null,
+		2,
+	)
 
-  res.setHeader('Content-Type', 'application/json')
-  res.setHeader('Content-Length', Buffer.byteLength(payload))
+	res.setHeader("Content-Type", "application/json")
+	res.setHeader("Content-Length", Buffer.byteLength(payload))
 
-  res.end(payload)
+	res.end(payload)
 })
 
-const port = process.env.PORT || 8080
-const host = process.env.HOST || '0.0.0.0'
-
-server.listen(port, host, () => {
-  console.log(`=> running at http://${host}:${port}`)
+server.listen(process.env.PORT || 5000, () => {
+	console.log(`=> running at http://${host}:${port}`)
 })
