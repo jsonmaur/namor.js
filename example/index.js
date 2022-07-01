f8eca8206a5e6fcbe34df32d0c0ae116c6bce37f
Ed Johnson-Williams <edjw@users.noreply.github.com>
2022-07-01 23:44:16 +0000

    Add guards for saltLength and number of words to example (#19)
    * change fallback host to localhost instead of 0.0.0.0
    
    * put correct path for example's package-lock.json
    
    * Make example only use maximum of 4 words
    
    * Add a maximum salt length of 20 to the example
    
    This only applies to the example and not to the generator. It stops the example crashing if somebody uses a saltLength that makes the generated_name longer than 63 characters.
    
    Co-authored-by: Ed Johnson-Williams <git@johnsonwilliams.co.uk>

--- example/index.js
+++ example/index.js
@@ -2,14 +2,17 @@ const http = require("http")
 const url = require("url")
 const namor = require("namor")
 
+const maxWords = 4
+const maxSaltLength = 20
+
 const server = http.createServer((req, res) => {
 	const { query } = url.parse(req.url, true)
 
 	const payload = JSON.stringify(
 		{
 			generated_name: namor.generate({
-				words: query.words,
-				saltLength: query.saltLength,
+				words: Math.min(query.words, maxWords),
+				saltLength: Math.min(query.saltLength, maxSaltLength),
 				saltType: query.saltType,
 				separator: query.separator,
 				subset: query.subset,
@@ -27,7 +30,7 @@ const server = http.createServer((req, res) => {
 })
 
 const port = process.env.PORT || 5000
-const host = process.env.HOST || "0.0.0.0"
+const host = process.env.HOST || "localhost"
 
 server.listen(port, host, () => {
 	console.log(`=> running at http://${host}:${port}`)
